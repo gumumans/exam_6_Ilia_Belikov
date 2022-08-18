2e6a1683a16ff0748b371600c816fee8a2449b46
Ilia Belikov <gumuman@mail.ru>
2022-08-18 12:00:57 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exam_6_Ilia_Belikov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
